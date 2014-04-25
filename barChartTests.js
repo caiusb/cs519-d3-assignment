71e1135a83806c6f122aac4d7cdad0e01d1f3740
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-04-25 15:17:00 -0700

    Extracted the svg into a global variable

--- barChartTests.js
+++ barChartTests.js
@@ -1,21 +1,22 @@
 var testDivDOM;
 var data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 var dataPointsNo = 10;
+var svg;
 
 QUnit.begin(function () {
 	var testDiv = d3.select("#test-div").datum(data);
 	console.log(testDiv)
 	barChart(testDiv);
 	testDivDOM = document.getElementById("test-div");
+	svg = testDivDOM.firstElementChild;
 });
 
 test("Test SVG Element Creation", function testSVGElement() {
-	var svg = testDivDOM.firstElementChild;
 	notEqual(svg, null, "Failed to create SVG element");
 	equal(svg.tagName, "svg", "Element type is not SVG");
 });
 
 test("Test Add Bars", function testAddBars() {
-	var svg = testDivDOM.firstElementChild;
 	equal(svg.children.length, dataPointsNo, "I should have " + dataPointsNo + " data points");
 });
+

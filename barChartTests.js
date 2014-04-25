ebbc3f3141250df754cb980d3f0c157193f23bfe
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-04-24 17:23:43 -0700

    Extracted the initial setup in the "begin" thing

--- barChartTests.js
+++ barChartTests.js
@@ -1,10 +1,13 @@
+QUnit.begin(function () {
+	test-div = document.getElementById("test-test-div");
+	notEqual(test-div, null, "The initial test-div must exist");
+});
+
 test("Test SVG Element Creation", function () {
-	var div = document.getElementById("test-div");
-	notEqual(div, null, "The initial div must exist");
 
-	barChart(div);
+	barChart(test-div);
 
-	var svg = div.firstElementChild;
+	var svg = test-div.firstElementChild;
 	notEqual(svg, null, "Failed to create SVG element");
 	equal(svg.tagName, "svg", "Element type is not SVG");
 });

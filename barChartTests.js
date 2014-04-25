4a86c4e4ec44afb5732316db999e4970b45e13cb
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-04-25 15:08:07 -0700

    The DOM node is not a global variable
    In the test suite.

--- barChartTests.js
+++ barChartTests.js
@@ -1,11 +1,14 @@
+var testDivDOM;
+
 QUnit.begin(function () {
 	var testDiv = d3.select("#test-div");
 	console.log(testDiv)
 	barChart(testDiv);
+	testDivDOM = document.getElementById("test-div");
 });
 
 test("Test SVG Element Creation", function testSVGElement() {
-	var svg = document.getElementById("test-div").firstElementChild;
+	var svg = testDivDOM.firstElementChild;
 	notEqual(svg, null, "Failed to create SVG element");
 	equal(svg.tagName, "svg", "Element type is not SVG");
 });

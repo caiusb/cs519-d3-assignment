6d639273e3bac6173c493fd58560ab10898b26ac
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-04-25 15:11:30 -0700

    Adding the bars
    Right now, they don't have a clear possition, or size or anything.

--- barChart.js
+++ barChart.js
@@ -1,5 +1,10 @@
 function barChart(selection) {
 	selection.each(function(data) {
 		var svg = selection.append("svg");
+		
+		svg.selectAll("rect")
+			.data(data)
+			.enter()
+			.append("rect");
 	});
 }
--- barChartTests.js
+++ barChartTests.js
@@ -1,7 +1,9 @@
 var testDivDOM;
+var data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var dataPointsNo = 10;
 
 QUnit.begin(function () {
-	var testDiv = d3.select("#test-div");
+	var testDiv = d3.select("#test-div").datum(data);
 	console.log(testDiv)
 	barChart(testDiv);
 	testDivDOM = document.getElementById("test-div");
@@ -11,4 +13,9 @@ test("Test SVG Element Creation", function testSVGElement() {
 	var svg = testDivDOM.firstElementChild;
 	notEqual(svg, null, "Failed to create SVG element");
 	equal(svg.tagName, "svg", "Element type is not SVG");
+});
+
+test("Test Add Bars", function testAddBars() {
+	var svg = testDivDOM.firstElementChild;
+	equal(svg.children.length, dataPointsNo, "I should have " + dataPointsNo + " data points");
 });

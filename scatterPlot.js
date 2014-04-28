fec2b1b1f5c1dac8568ac1fa1cbc5227ceb36d39
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-04-28 11:19:36 -0700

    Empty stubs for the scatter plot

---
+++ scatterPlot.js
@@ -0,0 +1,16 @@
+function scatterPlot() {
+
+	var height = 500;
+	var width = 900;
+
+	var margins = {top: 20, left: 30, right: 30, bottom: 20};
+
+	var chart = function(selection) {
+		selection.each(function(data) {
+			var svg = d3.select(this).append("svg");
+	  
+	});
+  }
+
+  return chart;
+}
